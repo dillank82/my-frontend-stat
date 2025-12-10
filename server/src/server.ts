105d4f6d27f97b602f41ae7dbdffd1f7e511503a
dillank82 <dillankyuppi@gmail.com>
2025-12-10 15:52:56 +0500

    add prod adress to cors options

--- server/src/server.ts
+++ server/src/server.ts
@@ -12,7 +12,7 @@ dotenv.config()
 const PORT = process.env.PORT || 3000
 
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'https://dillank-frontend-stat.vercel.app'],
     methods: ['GET', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
