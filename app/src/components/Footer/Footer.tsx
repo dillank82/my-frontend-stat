ec16f20669196b62b4eccfc435e34aff16cbded1
dillank82 <dillankyuppi@gmail.com>
2025-12-12 13:49:06 +0500

    implement LinkPointer logic inside Footer

--- app/src/components/Footer/Footer.tsx
+++ app/src/components/Footer/Footer.tsx
@@ -1,17 +1,23 @@
-import type { ReactElement } from "react";
+import { useRef, type ReactElement } from "react";
 import './Footer.css'
 import { FillingDots } from "../FillingDots/FillingDots";
+import { useIsElementVisible } from "../../hooks/useIsElementVisible/useIsElementVisible";
+import { LinkPointer } from "../LinkPointer/LinkPointer";
 
 interface FooterProps {
     portfolioLink: string
 }
 
 export const Footer = ({ portfolioLink }: FooterProps): ReactElement => {
+    const ref = useRef(null)
+    const isVisible = useIsElementVisible({ref, threshold: 0.01})
 
     return (
         <>
             <footer>
-                <span>my portfolio: <a href={portfolioLink}>{portfolioLink}</a></span>
+                <LinkPointer isVisible={isVisible}/>
+
+                <span ref={ref}>my portfolio: <a href={portfolioLink}>{portfolioLink}</a></span>
                 <FillingDots />
                 <FillingDots />
             </footer>
