c4947a580c760b9a56d680f40a21de0c57b12699
dillank82 <dillankyuppi@gmail.com>
2025-12-12 13:06:37 +0500

    create useIsElementVisible hook

---
+++ app/src/hooks/useIsElementVisible/useIsElementVisible.ts
@@ -0,0 +1,36 @@
+import { useEffect, useState, type RefObject } from "react"
+
+interface useIsElementVisibleProps {
+    ref: RefObject<HTMLElement | null>
+    threshold: number
+}
+
+export const useIsElementVisible = ({ ref, threshold }: useIsElementVisibleProps) => {
+    const [isVisible, setIsVisible] = useState(true)
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(
+            (entries) => {
+                if (entries[0]) {
+                    setIsVisible(entries[0].isIntersecting)
+                }
+            },
+            {
+                root: null,
+                rootMargin: '0px',
+                threshold: threshold
+            }
+        )
+        if (ref.current) {
+            observer.observe(ref.current)
+        }
+        
+        return () => {
+            if (ref.current) {
+                observer.unobserve(ref.current)
+            }
+        }
+    }, [ref, threshold])
+
+    return isVisible
+}
